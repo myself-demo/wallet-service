ca1988923f91051c2d9c004ffb7a5cfd42808967
Husky <husky.robot.dog@gmail.com>
2022-02-26 15:54:05 +0800

    init

--- src/main/java/com/github/myself_demo/wallet_service/Main.java
+++ src/main/java/com/github/myself_demo/wallet_service/Main.java
@@ -1,7 +1,16 @@
 package com.github.myself_demo.wallet_service;
 
-public class Main {
-    public static void main(String[] args) {
+import com.github.myself_demo.wallet_service.service.WalletServiceProvider;
+import io.grpc.ServerBuilder;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final var server = ServerBuilder.forPort(9002)
+                .addService(new WalletServiceProvider())
+                .build()
+                .start();
+        server.awaitTermination();
     }
 }
